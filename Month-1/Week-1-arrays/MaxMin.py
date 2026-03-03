3ad4eb48afb88461a9582e86fbce62598855d18f
Likitha <likithav62@gmail.com>
2026-03-03 10:42:49 +0530

    Day 1: Max and Min in array (Traversal basics)

---
+++ Month-1/Week-1-arrays/MaxMin.py
@@ -0,0 +1,15 @@
+# max in an array
+nums = [3,8,1,10,5]
+max = nums[0]
+for num in nums:
+    if num >= max:
+      max = num
+print(max)
+
+# min in an array
+nums = [3,8,1,10,5]
+min = nums[0]
+for num in nums:
+    if num <= min:
+      min = num
+print(min)
