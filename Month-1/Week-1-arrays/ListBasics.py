c8b9398f3143b77479a362f59629add7f4e38a8c
Likitha <likithav62@gmail.com>
2026-03-04 09:00:04 +0530

    list basics

--- Month-1/Week-1-arrays/ListBasics.py
+++ Month-1/Week-1-arrays/ListBasics.py
@@ -1,4 +1,4 @@
-# max in an array
+ #max in an array
 nums = [3,8,1,10,5]
 max = nums[0]
 for num in nums:
@@ -13,3 +13,11 @@ for num in nums:
     if num <= min:
       min = num
 print(min)
+
+# count even numbers in an array
+nums = [3,8,1,10,5]
+count_even = 0
+for num in nums:
+  if num%2 == 0:
+    count_even += 1
+print(count_even)
